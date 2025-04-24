7a15683352886b7f0c7815f320b70e912924c966
Ali Çömez | Slaweally <sys@rootali.net>
2025-04-24 20:49:48 +0300

    Update nexus.rc.php

--- nexus/nexus.rc.php
+++ nexus/nexus.rc.php
@@ -10,19 +10,13 @@
 defined('COT_CODE') or die('Wrong URL.');
 
 // Add original CSS files first
-Resources::addFile(Cot::$cfg['themes_dir'] . '/' . Cot::$usr['theme'] . '/css/reset.css');
-Resources::addFile(Cot::$cfg['themes_dir'] . '/' . Cot::$usr['theme'] . '/css/extras.css');
-Resources::addFile(Cot::$cfg['themes_dir'] . '/' . Cot::$usr['theme'] . '/css/nexus.css');
-Resources::addFile(Cot::$cfg['themes_dir'] . '/' . Cot::$usr['theme'] . '/css/modalbox.css');
-
-// Add our responsive improvements 
-Resources::addFile(Cot::$cfg['themes_dir'] . '/' . Cot::$usr['theme'] . '/css/responsive.css');
+Resources::addFile(Cot::$cfg['themes_dir'] . '/' . Cot::$usr['theme'] . '/css/modern.css');
 
 // Add Google Fonts (optional - remove if causes issues)
 Resources::addFile('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');
 
 // Add original JS
-Resources::addFile(Cot::$cfg['themes_dir'] . '/' . Cot::$usr['theme'] . '/js/nexus.js');
-
+Resources::addFile(Cot::$cfg['themes_dir'] . '/' . Cot::$usr['theme'] . '/js/modern.js');
+Resources::addFile(Cot::$cfg['themes_dir'] . '/' . Cot::$usr['theme'] . '/js/extra.js');
 // jQuery dependency for plugins (güvenli yöntem)
 Resources::linkFileFooter('js/jquery.min.js');
